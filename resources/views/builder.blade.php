589dcd92e130be8f2832a10266c89f77325c3c13
Hugh Messenger <hugh.messenger@gmail.com>
2023-12-20 22:06:29 -0600

    Adding widget

--- resources/views/builder.blade.php
+++ resources/views/builder.blade.php
@@ -9,65 +9,82 @@
     :required="$isRequired()"
     :state-path="$getStatePath()"
 >
-    @php
-        $treeId = $getId();
+{{--    @php--}}
+{{--        $treeId = $getId();--}}
 
-        $isAddable = $isAddable();
-        $isDeletable = $isDeletable();
-        $isDisabled = $isDisabled();
-        $isEditable = $isEditable();
-        $isReorderable = $isReorderable();
-        $maxDepth = $getMaxDepth();
-        $startCollapsed = $getStartCollapsed();
-        $addAction = $getAction('add');
-        $itemActions = [$getAction('addChild'), $getAction('delete'), $getAction('edit'), $getAction('reorder')];
-    @endphp
+{{--        $isAddable = $isAddable();--}}
+{{--        $isDeletable = $isDeletable();--}}
+{{--        $isDisabled = $isDisabled();--}}
+{{--        $isEditable = $isEditable();--}}
+{{--        $isReorderable = $isReorderable();--}}
+{{--        $maxDepth = $getMaxDepth();--}}
+{{--        $startCollapsed = $getStartCollapsed();--}}
+{{--        $addAction = $getAction('add');--}}
+{{--        $itemActions = [$getAction('addChild'), $getAction('delete'), $getAction('edit'), $getAction('reorder')];--}}
+{{--    @endphp--}}
 
-    <div wire:key="tree-items-wrapper">
-        <div
-            class="space-y-2"
-            data-sortable-container
-            ax-load
-            ax-load-css="{{ \Filament\Support\Facades\FilamentAsset::getStyleHref('filament-adjacency-list-styles', 'saade/filament-adjacency-list') }}"
-            ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-adjacency-list', 'saade/filament-adjacency-list') }}"
-            x-data="tree({
-                treeId: @js($treeId),
-                statePath: @js($getStatePath()),
-                disabled: @js($isDisabled),
-                maxDepth: @js($maxDepth)
-            })"
-        >
-            @forelse($getState() as $uuid => $item)
-                <x-filament-adjacency-list::item
-                    :tree-id="$treeId"
-                    :actions="$itemActions"
-                    :addable="$isAddable"
-                    :children-key="$getChildrenKey()"
-                    :deletable="$isDeletable"
-                    :disabled="$isDisabled"
-                    :editable="$isEditable"
-                    :start-collapsed="$startCollapsed"
-                    :item="$item"
-                    :item-state-path="$getStatePath() . '.' . $uuid"
-                    :label-key="$getLabelKey()"
-                    :reorderable="$isReorderable"
-                    :state-path="$getStatePath()"
-                    :max-depth="$maxDepth"
-                />
-            @empty
-                <div @class([
-                    'w-full bg-white rounded-lg border border-gray-300 px-3 py-2 text-left rtl:text-right',
-                    'dark:bg-gray-900 dark:border-white/10',
-                ])>
-                    {{ __('filament-adjacency-list::adjacency-list.items.empty') }}
-                </div>
-            @endforelse
-        </div>
-    </div>
+    <x-filament-adjacency-list::wrapper
+        :tree-id="$getId()"
+        :items="$getState()"
+        :state-path="$getStatePath()"
+        :itemActions="[$getAction('addChild'), $getAction('delete'), $getAction('edit'), $getAction('reorder')]"
+        :addAction="$getAction('add')"
+        :addable="$isAddable()"
+        :children-key="$getChildrenKey()"
+        :deletable="$isDeletable()"
+        :disabled="$isDisabled()"
+        :editable="$isEditable()"
+        :start-collapsed="$getStartCollapsed()"
+        :label-key="$getLabelKey()"
+        :reorderable="$isReorderable()"
+        :max-depth="$getMaxDepth()"
+    />
 
-    <div class="flex justify-end">
-        @if ($isAddable)
-            {{ $addAction(['statePath' => $getStatePath()]) }}
-        @endif
-    </div>
+{{--    <div wire:key="tree-items-wrapper">--}}
+{{--        <div--}}
+{{--            class="space-y-2"--}}
+{{--            data-sortable-container--}}
+{{--            ax-load--}}
+{{--            ax-load-css="{{ \Filament\Support\Facades\FilamentAsset::getStyleHref('filament-adjacency-list-styles', 'saade/filament-adjacency-list') }}"--}}
+{{--            ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-adjacency-list', 'saade/filament-adjacency-list') }}"--}}
+{{--            x-data="tree({--}}
+{{--                treeId: @js($treeId),--}}
+{{--                statePath: @js($getStatePath()),--}}
+{{--                disabled: @js($isDisabled),--}}
+{{--                maxDepth: @js($maxDepth)--}}
+{{--            })"--}}
+{{--        >--}}
+{{--            @forelse($getState() as $uuid => $item)--}}
+{{--                <x-filament-adjacency-list::item--}}
+{{--                    :tree-id="$treeId"--}}
+{{--                    :actions="$itemActions"--}}
+{{--                    :addable="$isAddable"--}}
+{{--                    :children-key="$getChildrenKey()"--}}
+{{--                    :deletable="$isDeletable"--}}
+{{--                    :disabled="$isDisabled"--}}
+{{--                    :editable="$isEditable"--}}
+{{--                    :start-collapsed="$startCollapsed"--}}
+{{--                    :item="$item"--}}
+{{--                    :item-state-path="$getStatePath() . '.' . $uuid"--}}
+{{--                    :label-key="$getLabelKey()"--}}
+{{--                    :reorderable="$isReorderable"--}}
+{{--                    :state-path="$getStatePath()"--}}
+{{--                    :max-depth="$maxDepth"--}}
+{{--                />--}}
+{{--            @empty--}}
+{{--                <div @class([--}}
+{{--                    'w-full bg-white rounded-lg border border-gray-300 px-3 py-2 text-left rtl:text-right',--}}
+{{--                    'dark:bg-gray-900 dark:border-white/10',--}}
+{{--                ])>--}}
+{{--                    {{ __('filament-adjacency-list::adjacency-list.items.empty') }}--}}
+{{--                </div>--}}
+{{--            @endforelse--}}
+{{--        </div>--}}
+{{--    </div>--}}
+
+{{--    <div class="flex justify-end">--}}
+{{--        @if ($isAddable)--}}
+{{--            {{ $addAction(['statePath' => $getStatePath()]) }}--}}
+{{--        @endif--}}
+{{--    </div>--}}
 </x-filament-forms::field-wrapper>
---
+++ src/Widgets/AdjacencyListWidget.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Saade\FilamentAdjacencyList\Widgets;
+
+use App\Models\Agencies\Agency;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
+use Filament\Widgets;
+use Illuminate\Database\Eloquent\Model;
+use Saade\FilamentAdjacencyList\Forms\Components\AdjacencyList;
+
+class AdjacencyListWidget extends Widgets\Widget implements HasForms
+{
+    use InteractsWithForms;
+
+    public ?array $data = [];
+
+    public Model $model;
+
+    protected static string $view = 'filament-adjacency-list::widget';
+
+    protected static ?string $heading = null;
+
+    protected static ?string $icon = 'heroicon-o-list-bullet';
+
+    protected static bool $collapsible = true;
+
+    protected static bool $startCollapsed = true;
+
+    public function mount(): void
+    {
+        $this->model = $this->getModel();
+
+        $this->form->fill(
+            $this->model->toArray()
+        );
+    }
+
+    public function getModel(): ?Model
+    {
+        return $this->model;
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Group::make()
+                    ->schema([
+                        AdjacencyList::make('kiddies')
+                            ->relationship('descendants')
+                            ->pivotAttributes(['agency_tree_type_id' => 1])
+                            ->labelKey('name')
+                            ->customPath('tree_path'),
+                    ])
+                    ->columns(1)
+
+            ])
+            ->statePath('data')
+            ->model($this->model);
+    }
+
+//    protected function getData(): array
+//    {
+//        return $this->getStateFromRelatedRecords(
+//            collect()
+//        );
+//    }
+
+    /**
+     * @return array<array<string, mixed>>
+     */
+//    protected function getStateFromRelatedRecords(Collection $records): array
+//    {
+//        if (! $records->count()) {
+//            return [];
+//        }
+//
+//        $state = [];
+//
+//        $path = $this->getPath();
+//        //        $translatableContentDriver = $this->getLivewire()->makeFilamentTranslatableContentDriver();
+//
+//        $records
+//            ->each(
+//                function (Model $record) use (&$state, $path): void {
+//                    $data = $record->attributesToArray();
+//
+//                    //                    $data = $this->mutateRelationshipDataBeforeFill($data);
+//
+//                    // Depending on the records order, a children can be created before its parent.
+//                    // In this case, we need to merge the children with the parent data.
+//                    $key = $record->{$path};
+//
+//                    if ($existing = data_get($state, $key)) {
+//                        data_set($state, $key, array_merge($existing, $data));
+//                    } else {
+//                        data_set($state, $key, $data);
+//                    }
+//                }
+//            );
+//
+//        return $state;
+//    }
+
+    public function getCollapsible(): bool
+    {
+        return static::$collapsible;
+    }
+
+    public function getStartCollapsed(): bool
+    {
+        return static::$startCollapsed;
+    }
+
+    protected function getHeading(): ?string
+    {
+        return static::$heading;
+    }
+
+    protected function getIcon(): ?string
+    {
+        return static::$icon;
+    }
+}
